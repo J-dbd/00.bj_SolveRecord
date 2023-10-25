220a3d9d8caa5bc08ac5088627976c796556615d
J_ManG <kjamong2020@gmail.com>
2023-10-25 19:36:59 +0900

    checkbox check

--- silver/11724. 연결 요소의 개수/11724 stack.py
+++ silver/11724. 연결 요소의 개수/11724 stack.py
@@ -1,6 +1,6 @@
 import sys 
-from collections import deque
 V, E= list(map(int, sys.stdin.readline().strip().split()))
+
 
 graph=[[] for _ in range(V+1)]
 
@@ -8,20 +8,27 @@ for i in range(E):
     s, e =list(map(int, sys.stdin.readline().strip().split()))
     graph[s].append(e)
     graph[e].append(s)
-
+    
+stack=[]
 visited=[False]*(V+1)
 cnt=0
 
-def DFS(v):
-    visited[v]=1
+def DFS_stack(v, stack, visited):
+    stack.append(v)
+    visited[v]=True
     
-    for now in graph[v]:
-        if not visited[now]:
-            DFS(now)
+    while stack:
+        now=stack.pop()
         
+        for vtx in graph[now]:
+            if not visited[vtx]:
+                stack.append(vtx)
+                visited[vtx]=True
+    
+
 for i in range(1, V+1):
     if not visited[i]:
-        DFS(i)
+        DFS_stack(i, stack, visited)
         cnt+=1
-    
+
 print(cnt)
