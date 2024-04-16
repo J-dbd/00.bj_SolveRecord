44bca765ad91483e54bce33235e87081f302a8a0
J_ManG <kjamong2020@gmail.com>
2024-04-16 23:44:49 +0900

    cd01

---
+++ eng_codingTest/codility/01. binarygap.ts
@@ -0,0 +1,7 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+//number
+function solution(N: number): void {
+  // Implement your solution here
+}
---
+++ silver/2331. 반복수열/test.js
@@ -0,0 +1,6 @@
+const numberArray = [40, 1, 5, 200];
+
+numberArray.sort();
+
+console.log(numberArray);
+//[ 1, 200, 40, 5 ]
